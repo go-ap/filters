75d2c3db9b8b5b1d73fa0d9d2095989286526e8e
Marius Orcsik <marius@federated.id>
2023-10-29 10:52:34 +0100

    More tests for Any and All

--- aggregators_test.go
+++ aggregators_test.go
@@ -6,26 +6,81 @@ import (
 	vocab "github.com/go-ap/activitypub"
 )
 
+func _mockTrue(_ vocab.Item) bool {
+	return true
+}
+
+func _mockFalse(_ vocab.Item) bool {
+	return false
+}
+
 func TestAny(t *testing.T) {
-	type args struct {
-		fns []Fn
-	}
 	tests := []struct {
 		name string
-		args args
+		fns  []Fn
 		want bool
 	}{
 		{
-			name: "empty",
-			args: args{},
+			name: "empty is false",
+			want: false,
+		},
+		{
+			name: "one true, one false",
+			fns:  []Fn{_mockTrue, _mockFalse},
+			want: true,
+		},
+		{
+			name: "all true",
+			fns:  []Fn{_mockTrue, _mockTrue},
+			want: true,
+		},
+		{
+			name: "all false",
+			fns:  []Fn{_mockFalse, _mockFalse},
 			want: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ob := vocab.Object{}
-			if got := Any(tt.args.fns...)(ob); got != tt.want {
+			if got := Any(tt.fns...)(ob); got != tt.want {
 				t.Errorf("Any() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAll(t *testing.T) {
+	tests := []struct {
+		name string
+		fns  []Fn
+		want bool
+	}{
+		{
+			name: "empty is false",
+			want: false,
+		},
+		{
+			name: "one true, one false",
+			fns:  []Fn{_mockTrue, _mockFalse},
+			want: false,
+		},
+		{
+			name: "all true",
+			fns:  []Fn{_mockTrue, _mockTrue},
+			want: true,
+		},
+		{
+			name: "all false",
+			fns:  []Fn{_mockFalse, _mockFalse},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ob := vocab.Object{}
+			if got := All(tt.fns...)(ob); got != tt.want {
+				t.Errorf("All() = %v, want %v", got, tt.want)
 			}
 		})
 	}
