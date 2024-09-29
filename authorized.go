2076c77bcc14f571e1a511af9d96ec94c5246c0e
Marius Orcsik <marius@federated.id>
2024-09-29 13:02:48 +0200

    Updated authorized filter for checking the public namespace

--- authorized.go
+++ authorized.go
@@ -8,10 +8,10 @@ func (a authorized) Apply(it vocab.Item) bool {
 	if vocab.IsNil(it) {
 		return false
 	}
-	if vocab.IRI(a).Equals(vocab.PublicNS, true) {
+	aud := fullAudience(it)
+	if aud.Count() == 0 && vocab.IRI(a).Equals(vocab.PublicNS, true) {
 		return true
 	}
-	aud := fullAudience(it)
 	return aud.Contains(vocab.PublicNS) || aud.Contains(vocab.IRI(a))
 }
 
